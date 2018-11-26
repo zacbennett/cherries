b3bbdde70980038c82536fa2bb1ef506946ffd5f
NEsfandiari <nesfandiari37@gmail.com>
2018-11-26 10:50:27 -0800

    cherries first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,8 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Cherries</h1>
+    <p>Welcome to Cherries.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
