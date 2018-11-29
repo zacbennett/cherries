3e7a682d032ce6bfa671261ba479da7462add02d
JUAN ARECES <arecesj@gmail.com>
2018-11-28 16:15:34 -0800

    added scaffolding for ProductPages

---
+++ src/components/ProductPageContainer.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import Styled from 'styled-components'
+import NavBar from '../components/NavBar'
+import { graphql } from 'gatsby'
+
+/** Overview of index.js component:
+ *  - Created a styled div container which will wrap our NavBar component and, later on, our app.
+ *
+ */
+
+const Container = Styled.div`
+  align-items: center;
+  animation: fadein 1s; 
+  @keyframes fadein {
+        from { opacity: 0; }
+        to   { opacity: 1; }
+    }
+`
+
+const ProductPageContainer = ({ data }) => {
+  return (
+    <Container>
+      <NavBar />
+    </Container>
+  )
+}
+
+// Query contentful for products limiting to fresh picks (top four most recently created products)
+// Passed into ProductList component
+// Eventually may connect to shopify for sales-driven data
+export const query = graphql`
+  {
+    allContentfulProductPage(
+      sort: { fields: [createdAt], order: DESC }
+      limit: 4
+    ) {
+      edges {
+        node {
+          id
+          createdAt
+          title
+          price
+          images {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default ProductPageContainer
