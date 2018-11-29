ea04198e37fd87fa1cdfdb0eadb9c02ae0bb4bb2
Zac B <zacbennett0@gmail.com>
2018-11-29 14:22:10 -0800

    cleaned up file system

--- src/components/atoms/HomePageButton.jsx
+++ src/components/atoms/HomePageButton.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Styled from 'styled-components'
-
+import { Link } from 'gatsby'
 /** Overview of index.js component:
  *  - Created a styled div container which will wrap our NavBar component and, later on, our app.
  *
  */
 
 const Container = Styled.div`
+#hero-image-button{
 background-color: #e20031;
 color: white;
 text-transform: uppercase;
@@ -15,16 +16,19 @@ font-size: 23px;
 border: none;
 width: 100%;
 cursor: pointer;
+}
 `
 
-const HomePageButton = ({ data }) => {
-  return (
-    <Container>
-      <Link to="/">
-        <button id="hero-image-button">{this.props.buttonText}</button>
-      </Link>
-    </Container>
-  )
+class HomePageButton extends Component {
+  render() {
+    return (
+      <Container>
+        <Link to="/">
+          <button id="hero-image-button">{this.props.buttonText}</button>
+        </Link>
+      </Container>
+    )
+  }
 }
 
 export default HomePageButton
