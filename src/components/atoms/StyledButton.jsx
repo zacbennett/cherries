98367be80e3d959ff6335cc04a5e7115bc7cd422
Kenny Hwu <kennywhwu@gmail.com>
2018-11-30 10:54:45 -0800

    started signup component

---
+++ src/components/atoms/StyledButton.jsx
@@ -0,0 +1,29 @@
+import Styled from 'styled-components'
+
+const StyledButton = Styled.button`
+    margin: ${({ margin }) => margin || '1rem'};
+    padding: 0;
+    height: ${({ height }) => height || '3rem'};
+    width: ${({ width }) => width || '7rem'};
+    font-size: ${({ fontSize }) => fontSize || '.9rem'};
+    font-weight: ${({ fontWeight }) => fontWeight || 'normal'};
+    letter-spacing: .1rem;
+    color: ${({ color }) => color || 'white'};
+    background-color: ${({ backgroundColor }) => backgroundColor || '#FF0086'};
+    border-color: ${({ borderColor }) => borderColor || '#FF0086'};
+    outline: none;
+    border-radius: ${({ borderRadius }) => borderRadius || '2px'};
+    border-width: ${({ borderWidth }) => borderWidth || '2px'};
+    transition: 0.3s ease;
+    cursor: pointer;
+    
+
+  :hover{
+    background-color: ${({ hovercolor }) => hovercolor || 'white'};
+    color: ${({ hoverTextColor }) => hoverTextColor || '#FF0086'};
+  }
+  @media (max-width: 420px) {
+  }
+`
+
+export default StyledButton
---
+++ src/components/molecules/SignupEmailPassword.jsx
@@ -0,0 +1,152 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { StyledInput, StyledButton, Loading } from '../atoms'
+
+const Container = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  /* height: 14rem; */
+  .name {
+    display: flex;
+    justify-content: space-between;
+    width: 24.2rem;
+  }
+  .radio {
+    margin-top: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 17rem;
+    input {
+      margin: 0;
+      height: 1.2rem;
+    }
+    label {
+      font-family: Lato;
+      font-size: 14px;
+    }
+  }
+`
+
+class SignupEmailPassword extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    newsletter: false,
+    status: 'SIGN UP',
+  }
+  static contextTypes = {
+    firebase: PropTypes.object,
+  }
+  handleChange = e => {
+    if (e.target.type === 'radio') {
+      let newsState = !this.state.newsletter
+      this.setState({
+        newsletter: newsState,
+      })
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value,
+      })
+    }
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    const { firebase } = this.context
+    const { firstName, lastName, email, password, newsletter } = this.state
+    this.setState({ status: <Loading /> })
+    try {
+      firebase.signupEmailPassword(
+        this,
+        firstName,
+        lastName,
+        email,
+        password,
+        newsletter
+      )
+    } catch (err) {
+      console.log(err)
+      this.setState({ status: 'FAILURE' })
+    }
+  }
+  render() {
+    return (
+      <Container onSubmit={this.handleSubmit}>
+        <div className="name">
+          <StyledInput
+            aria-label="First Name"
+            type="text"
+            width="10.5rem"
+            placeholder="First Name"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+            marginRight="1.3em"
+            fontFamily="Lato"
+          />
+          <StyledInput
+            aria-label="Last Name"
+            type="text"
+            width="10.5rem"
+            placeholder="Last Name"
+            name="lastName"
+            onChange={this.handleChange}
+            value={this.state.lastName}
+            marginLeft="1.3em"
+            fontFamily="Lato"
+          />
+        </div>
+        <StyledInput
+          type="email"
+          aria-label="Email Address"
+          placeholder="Email"
+          name="email"
+          onChange={this.handleChange}
+          value={this.state.email}
+          width="22rem"
+          fontFamily="Lato"
+        />
+        <StyledInput
+          placeholder="Password"
+          aria-label="Password"
+          name="password"
+          onChange={this.handleChange}
+          value={this.state.password}
+          width="22rem"
+          fontFamily="Lato"
+          marginBottom="2em"
+        />
+        <StyledButton
+          backgroundColor="#E20031"
+          borderColor="#E20031"
+          fontWeight="700"
+          fontSize="16px"
+          width="23.2rem"
+          height="2.2rem"
+          margin="0"
+        >
+          {this.state.status}
+        </StyledButton>
+        <div className="radio">
+          <StyledInput
+            aria-label="Newsletter"
+            width="2rem"
+            type="radio"
+            onClick={this.handleChange}
+            checked={this.state.newsletter}
+            name="newsletter"
+            id="newsletter"
+          />
+          <label for="newsletter"> I'd like to hear from Lipslut</label>
+        </div>
+      </Container>
+    )
+  }
+}
+
+export default SignupEmailPassword
