df8fe24136ea7a56c3e38dd97ea0f8d0109c5fa9
JUAN ARECES <arecesj@gmail.com>
2018-12-03 10:00:50 -0800

    stashin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Styled from 'styled-components'
-import { BannerPromo, NavButtons } from './molecules/'
+import BannerPromo from './molecules/BannerPromo'
+import NavButtons from './molecules/NavButtons'
 import { StaticQuery, graphql } from 'gatsby'
 
 /** Overview of NavBar component:
+ *  - Created a styled div NavContainer which wraps our NavBar
  *  - NavBar renders NavButtons and Banner Promo
  *  - NavButtons and Banner Promo will be passed down props such as icons and text that are queried dynamically from contentful
+ *
  */
-
+//Another comment
 const NavContainer = Styled.div`
   position: relative;
   top: 0;
