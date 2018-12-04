8ca47d19d683867fd90e42b7b121ef445b17a272
JUAN ARECES <arecesj@gmail.com>
2018-12-03 17:25:12 -0800

    joel fixed our bug. bless up

--- src/templates/ProductTemplate.js
+++ src/templates/ProductTemplate.js
@@ -18,7 +18,9 @@ class ProductTemplate extends Component {
     const data = this.props.pageContext.node
     const images = data.images.map(img => img.file.url)
 
-    const details = data.details.content.map(c => c.content[0].value)
+    const details = data.details
+      ? data.details.content.map(c => c.content[0].value)
+      : []
     return (
       <MainLayout>
         <SideNav />
