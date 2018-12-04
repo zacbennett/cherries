fa8943d7450d2988444d099cd94ced393f592039
Hasier Pastor <hasier.pastor@gmail.com>
2018-12-03 17:15:46 -0800

    resolved huge auth bug - signup auth now working

--- lambda-src/newAccount.js
+++ lambda-src/newAccount.js
@@ -5,15 +5,18 @@ const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
 }
+//although api said to use application/graphql we had to use application/json because we are using variables passed in as an object
 const shopifyConfig = {
-  'Content-Type': 'application/graphql',
-  'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_STOREFRONT_KEY,
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+  'X-Shopify-Access-Token': process.env.SHOPIFY_API_PASSWORD,
 }
-exports.handler = async function(event, context, callback) {
-  // TEST for POST request
-  console.log('NEWACCOUNT, WORKING')
+
+exports.handler = function(event, context, callback) {
+  //make sure request is secure - CORS
+  //sends an option method request
   if (event.httpMethod !== 'POST' || !event.body) {
-    callback(null, {
+    return callback(null, {
       statusCode,
       headers,
       body: '',
@@ -21,10 +24,9 @@ exports.handler = async function(event, context, callback) {
   }
   if (event.body[0] == '{') {
     let data = JSON.parse(event.body)
-    data = JSON.parse(data.body)
+    let body = JSON.parse(data.body)
 
-    const payload = {
-      query: `mutation customerCreate($input: CustomerInput!) {
+    const query = `mutation customerCreate($input: CustomerInput!) {
         customerCreate(input: $input) {
           userErrors {
             field
@@ -34,45 +36,47 @@ exports.handler = async function(event, context, callback) {
             id
           }
         }
-      }`,
-      variables: {
-        input: {
-          email: data.email,
-          password: data.password,
-          acceptsMarketing: data.newsletter,
-          firstName: data.firstName,
-          lastName: data.lastName,
-        },
+      }`
+
+    //shopify-admin api doesn't accept password as a field
+    const variables = {
+      input: {
+        email: body.email,
+        acceptsMarketing: body.newsletter,
+        firstName: body.firstName,
+        lastName: body.lastName,
       },
     }
 
-    try {
-      let customer = await axios({
-        url: 'https://cherries2018.myshopify.com/admin/api/graphql.json',
-        method: 'POST',
-        headers: shopifyConfig,
-        data: payload,
+    axios
+      .post(
+        'https://cherries2018.myshopify.com/admin/api/graphql.json',
+        {
+          variables,
+          query,
+        },
+        { headers: shopifyConfig }
+      )
+      .then(function(data) {
+        let customer
+        if (data.data.errors) {
+          return callback(data.data.errors)
+        } else {
+          customer = data.data.customerCreate
+        }
+        // console.log('what is customer?', customer)
+        let response = {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify({
+            customer,
+          }),
+        }
+        return callback(null, response)
       })
-      if (customer.data.errors.length > 0) customer = customer.data.errors
-      else customer = customer.data.data.customerCreate
-      let response = {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({
-          customer,
-        }),
-      }
-      callback(null, response)
-    } catch (err) {
-      console.log(err)
-      let response = {
-        statusCode: 500,
-        headers,
-        body: JSON.stringify({
-          error: err.message,
-        }),
-      }
-      callback(null, response)
-    }
+      .catch(err => {
+        console.log('ERROR!', err.response.data)
+        return callback(err)
+      })
   }
 }
