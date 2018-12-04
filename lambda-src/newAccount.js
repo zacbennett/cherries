44616251584c0fae5eac665478fccadf8cc492a2
Kenny Hwu <kennywhwu@gmail.com>
2018-12-04 10:57:13 -0800

    adding shopify storefront api

--- lambda-src/newAccount.js
+++ lambda-src/newAccount.js
@@ -6,10 +6,16 @@ const headers = {
   'Access-Control-Allow-Headers': 'Content-Type',
 }
 //although api said to use application/graphql we had to use application/json because we are using variables passed in as an object
-const shopifyConfig = {
+const shopifyAdminConfig = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
   'X-Shopify-Access-Token': process.env.SHOPIFY_API_PASSWORD,
+}
+
+const shopifyStorefrontConfig = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+  'X-Shopify-Access-Token': process.env.SHOPIFY_STOREFRONT_KEY,
 }
 
 exports.handler = function(event, context, callback) {
@@ -26,7 +32,7 @@ exports.handler = function(event, context, callback) {
     let data = JSON.parse(event.body)
     let body = JSON.parse(data.body)
 
-    const query = `mutation customerCreate($input: CustomerInput!) {
+    const adminQuery = `mutation customerCreate($input: CustomerInput!) {
         customerCreate(input: $input) {
           userErrors {
             field
@@ -39,7 +45,7 @@ exports.handler = function(event, context, callback) {
       }`
 
     //shopify-admin api doesn't accept password as a field
-    const variables = {
+    const adminVariables = {
       input: {
         email: body.email,
         acceptsMarketing: body.newsletter,
@@ -48,14 +54,15 @@ exports.handler = function(event, context, callback) {
       },
     }
 
+    // Shopify Admin API call
     axios
       .post(
         'https://cherries2018.myshopify.com/admin/api/graphql.json',
         {
-          variables,
-          query,
+          query: adminQuery,
+          variables: adminVariables,
         },
-        { headers: shopifyConfig }
+        { headers: shopifyAdminConfig }
       )
       .then(function(data) {
         let customer
@@ -78,5 +85,63 @@ exports.handler = function(event, context, callback) {
         console.log('ERROR!', err.response.data)
         return callback(err)
       })
+
+    //   const storefrontQuery = `mutation customerCreate($input: CustomerCreateInput!) {
+    //   customerCreate(input: $input) {
+    //     userErrors {
+    //       field
+    //       message
+    //     }
+    //     customer {
+    //       id
+    //     }
+    //     customerUserErrors {
+    //       field
+    //       message
+    //     }
+    //   }
+    // }`
+
+    //   const storefrontVariables = {
+    //     input: {
+    //       email: body.email,
+    //       password: body.password,
+    //       acceptsMarketing: body.newsletter,
+    //       firstName: body.firstName,
+    //       lastName: body.lastName,
+    //     },
+    //   }
+
+    //   // Shopify Storefront API call
+    //   axios
+    //     .post(
+    //       'https://cherries2018.myshopify.com/api/graphql',
+    //       {
+    //         query: storefrontQuery,
+    //         variables: storefrontVariables,
+    //       },
+    //       { headers: shopifyStorefrontConfig }
+    //     )
+    //     .then(function(data) {
+    //       let customer
+    //       if (data.data.errors) {
+    //         return callback(data.data.errors)
+    //       } else {
+    //         customer = data.data.customerCreate
+    //       }
+    //       // console.log('what is customer?', customer)
+    //       let response = {
+    //         statusCode: 200,
+    //         headers,
+    //         body: JSON.stringify({
+    //           customer,
+    //         }),
+    //       }
+    //       return callback(null, response)
+    //     })
+    //     .catch(err => {
+    //       console.log('ERROR!', err.response.data)
+    //       return callback(err)
+    //     })
   }
 }
