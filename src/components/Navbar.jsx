73b78075128e2fd847499d68012fff538bdf9080
Zac B <zacbennett0@gmail.com>
2018-12-04 09:14:44 -0800

    fixing navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import Styled from 'styled-components'
+import { StaticQuery, graphql } from 'gatsby'
+
+import { BannerPromo, NavButtons } from './molecules'
+
+const Container = Styled.div`
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+`
+export default () => (
+  <StaticQuery
+    query={graphql`
+      {
+        contentfulHomePage(pageTitle: { eq: "Home Page" }) {
+          navHelpIcon {
+            fluid {
+              src
+            }
+          }
+          navUserIcon {
+            fluid {
+              src
+            }
+          }
+          navCartIcon {
+            fluid {
+              src
+            }
+          }
+          navSearchIcon {
+            fluid {
+              src
+            }
+          }
+          navCherriesIcon {
+            fluid {
+              src
+            }
+          }
+          bannerPromoText
+          bannerPromoTitle
+          userLinks {
+            data {
+              dropdownLinks {
+                name
+                route
+              }
+            }
+          }
+          helpLinks {
+            data {
+              dropdownLinks {
+                name
+                route
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Container>
+        <BannerPromo
+          bannerText={data.contentfulHomePage.bannerPromoText}
+          bannerTitle={data.contentfulHomePage.bannerPromoTitle}
+        />
+        <NavButtons
+          helpIcon={data.contentfulHomePage.navHelpIcon.fluid.src}
+          userIcon={data.contentfulHomePage.navUserIcon.fluid.src}
+          searchIcon={data.contentfulHomePage.navSearchIcon.fluid.src}
+          cartIcon={data.contentfulHomePage.navCartIcon.fluid.src}
+          cherriesIcon={data.contentfulHomePage.navCherriesIcon.fluid.src}
+          userLinks={data.contentfulHomePage.userLinks.data}
+          helpLinks={data.contentfulHomePage.helpLinks.data}
+        />
+      </Container>
+    )}
+  />
+)
