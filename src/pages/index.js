8607e3225a2caa3cd4880cdbbbb98da1760b9ee3
Hasier Pastor <hasier.pastor@gmail.com>
2018-12-11 11:55:25 -0800

    added functionality for dynamically rendering different types on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import Styled from 'styled-components'
 import { graphql } from 'gatsby'
-
 import { HomePageHero, HomePageTryptych, SideNav } from '../components'
 import { ProductList } from '../components/molecules'
 import { MainLayout } from '../components/layouts'
+let Fuse = require('fuse.js')
 
 const Container = Styled.div`
   align-items: center;
@@ -24,24 +24,47 @@ class IndexPage extends Component {
     super(props)
     this.state = {}
   }
+
   render() {
+    const options = {
+      shouldSort: true,
+      threshold: 0.3,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: ['node.tags'],
+    }
     // loop over text in featured
     // map and render productList passing in products with search query
     // filter with search
     // get first product list to render between homepagehero and homepage tryptych
     // render rest of them after tryptych
-    const freshPicks = this.props.data.allContentfulProductPage.edges
+    const allProducts = this.props.data.allContentfulProductPage.edges
+    const typeArray = this.props.data.allContentfulHomePage.edges[0].node
+      .childContentfulHomePageFeaturedRichTextNode.content
+    let fuse = new Fuse(allProducts, options)
+    let productListComponents = typeArray.map(content => {
+      let searchResults = fuse.search(content.content[0].value)
+      console.log('SEARCH RESULTS', searchResults)
+      searchResults.length = 4
+      return (
+        <ProductList
+          products={searchResults}
+          title={content.content[0].value}
+        />
+      )
+    })
+
     return (
       <Container>
         <MainLayout>
           <SideNav className="sideNav" />
           <div>
             <HomePageHero />
-            <ProductList products={freshPicks} />
+            <ProductList products={allProducts.slice(0, 4)} />
             <HomePageTryptych />
-            <ProductList products={freshPicks} />
-            <ProductList products={freshPicks} />
-            <ProductList products={freshPicks} />
+            {productListComponents}
           </div>
         </MainLayout>
       </Container>
@@ -55,19 +78,30 @@ class IndexPage extends Component {
 // query for what are the featured items
 export const query = graphql`
   {
-    allContentfulProductPage(
-      sort: { fields: [createdAt], order: DESC }
-      limit: 4
-    ) {
+    allContentfulProductPage(sort: { fields: [createdAt], order: DESC }) {
       edges {
         node {
           id
           createdAt
           title
+          tags
           price
           images {
             file {
               url
+            }
+          }
+        }
+      }
+    }
+    allContentfulHomePage {
+      edges {
+        node {
+          childContentfulHomePageFeaturedRichTextNode {
+            content {
+              content {
+                value
+              }
             }
           }
         }
